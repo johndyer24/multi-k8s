951b437f1ae5d97acbe30798423543f27597637e
johndyer24 <johndyer24@gmail.com>
2020-04-25 16:48:55 -0400

    Add deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
-docker build -t johndyer24/multi-docker-client -f ./client/Dockerfile ./client
-docker build -t johndyer24/multi-docker-server -f ./server/Dockerfile ./server
-docker build -t johndyer24/multi-docker-worker -f ./worker/Dockerfile ./worker
-docker push johndyer24/multi-docker-client
-docker push johndyer24/multi-docker-server
-docker push johndyer24/multi-docker-worker
+docker build -t johndyer24/multi-docker-client:latest -t johndyer24/multi-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t johndyer24/multi-docker-server:latest -t johndyer24/multi-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t johndyer24/multi-docker-worker:latest -t johndyer24/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push johndyer24/multi-docker-client:latest
+docker push johndyer24/multi-docker-server:latest
+docker push johndyer24/multi-docker-worker:latest
+docker push johndyer24/multi-docker-client:$SHA
+docker push johndyer24/multi-docker-server:$SHA
+docker push johndyer24/multi-docker-worker:$SHA
 kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=johndyer24/multi-docker-server:$SHA
+kubectl set image deployments/client-deployment client=johndyer24/multi-docker-client:$SHA
+kubectl set image deployments/worker-deployment worker=johndyer24/multi-docker-worker:$SHA
